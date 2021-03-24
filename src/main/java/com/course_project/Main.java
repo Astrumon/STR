1b7728404ecae7cd249b940fbe62f4e6d71d33c5
Bidnyk Danil <dnlbdnk@gmail.com>
2021-03-24 18:05:29 +0200

    change name programm

--- src/main/java/com/course_project/Main.java
+++ src/main/java/com/course_project/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("basisUserInterface.fxml"));//com/course_project/UI/interface/basisUserInterface.fxml
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("SPRT");
         primaryStage.setScene(new Scene(root, 1100, 700));
         primaryStage.show();
         primaryStage.setResizable(false);
